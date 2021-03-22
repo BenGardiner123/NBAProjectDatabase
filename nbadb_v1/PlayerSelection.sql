fbb1c40579ffe8c4d9897d6ce13dcbe54d2c3a87
Ben Gardiner <bennygards@gmail.com>
2021-03-22 16:14:28 +1100

    Table Structure and dummy data

---
+++ nbadb_v1/PlayerSelection.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[PlayerSelection]
+(
+	[TeamName]          NVARCHAR(50)   Not Null, 
+    [Season]            INT            NOT NULL,
+    [PLAYER_ID]         INT            NOT NULL,
+    [TEAM_ABBREVIATION] VARCHAR (3)    NOT NULL,
+    primary key (TeamName, Season, TEAM_ABBREVIATION , Player_Id),
+    Foreign key (TeamName) references Team,
+    Foreign key (Season, [TEAM_ABBREVIATION], Player_ID) references NBAPlayer,
+   
+);
---
+++ nbadb_v1/Team.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Team]
+(
+	[TeamName] NVARCHAR(50) NOT NULL, 
+	primary key (TeamName),
+)
