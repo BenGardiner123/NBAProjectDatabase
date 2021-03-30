e4a68891ac795c15229ab8e067892bff1be5234a
Ben Gardiner <bennygards@gmail.com>
2021-03-30 16:38:45 +1100

    adding the stored procedure + usage e.g

--- nbadb_v1/allPlayers.sql
+++ nbadb_v1/allPlayers.sql
@@ -1,5 +1,5 @@
 ﻿create view allPlayers as 
-select Season,TEAM_ABBREVIATION, PLAYER_NAME ,AGE, GP, MINS, PLUS_MINUS, AST, BLK, BLKA, OREB, DREB, FG_PCT, 
+select PLAYER_NAME ,AGE, GP, MINS, PLUS_MINUS, AST, BLK, BLKA, OREB, DREB, FG_PCT, 
 FG3_PCT, FG3A, FG3M, FGA, FGM, FT_PCT, FTA, FTM, 
 W, L, W_PCT, PF, PFD
 from (select Season, PLAYER_NAME, TEAM_ABBREVIATION ,AGE, GP, MINS, PLUS_MINUS, AST, BLK, BLKA, OREB, DREB, FG_PCT, 
@@ -7,3 +7,4 @@ FG3_PCT, FG3A, FG3M, FGA, FGM, FT_PCT, FTA, FTM,
 W, L, W_PCT, PF, PFD, RANK() over (PARTITION by player_name order by season DESC) n
 from Player
 )m where n = 1
+
---
+++ nbadb_v1/exampleUsageOfTheStoredProc.sql
@@ -0,0 +1,3 @@
+﻿begin
+exec ViewAllPlayers @pageNumber = 1,  @RowsOfPage = 100, @SortingCol = 'PLAYER_NAME', @SortType = 'DESC';
+end
